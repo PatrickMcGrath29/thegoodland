0a7a8ca6ed6f4cf9c1c9436b5b29a59a98ddf8ba
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-02-08 21:29:42 -0500

    Some tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ const posts = await usePosts()
   <Container>
     <Hero />
     <div class="my-10 gap-5">
-      <h2 class="text-3xl font-medium mb-10 text-center">
+      <h2 class="text-3xl font-bold mb-10 text-center">
         Recent Posts
       </h2>
       <PostPreview v-for="post in posts" :key="post.slug" :post="post" />
