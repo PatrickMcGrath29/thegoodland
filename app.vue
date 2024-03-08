3d78043a7866c4a16a8204b7b6db589ede18095e
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-03-07 22:40:05 -0500

    Improve stat counter config

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const { public: { scProject, scSecurity } } = useRuntimeConfig()
+
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `The Good Land - ${titleChunk}` : 'The Good Land'
@@ -6,6 +8,13 @@ useHead({
   htmlAttrs: {
     lang: 'en',
   },
+  script: [
+    { src: 'https://www.statcounter.com/counter/counter.js', async: true, tagPosition: 'bodyClose' },
+    {
+      children: `var sc_project=${scProject};  var sc_invisible=1;  var sc_security=\'${scSecurity}\'; `,
+      tagPosition: 'bodyClose',
+    },
+  ],
 })
 </script>
 
@@ -15,4 +24,11 @@ useHead({
     <NuxtPage />
     <Footer />
   </div>
+  <noscript>
+    <div class='statcounter'>
+    <a title='StatCounter' href='https://statcounter.com/' target='_blank'>
+    <img class='statcounter' :src='`https://c.statcounter.com/${scProject}/0/${scSecurity}/1/`' alt='Statcounter' referrerPolicy='no-referrer-when-downgrade'>
+    </a>
+    </div>
+  </noscript>
 </template>
