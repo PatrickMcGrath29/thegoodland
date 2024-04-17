d439430f5ba0178362c2e08357930761cb547268
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-04-17 19:00:23 -0400

    Simplify inheritance stuff

--- components/content/Pullquote.vue
+++ components/content/Pullquote.vue
@@ -1,13 +1,7 @@
 <template>
   <div
-    class="
-    float-right w-full sm:w-[300px]
-    border-y border-neutral-500
-    py-8 !ml-10 !mr-0 my-10
-    [&>*]:mb-0 [&>*]:text-2xl [&>*]:italic
-    [&>*]:before:content-['“'] [&>*]:after:content-['”']
-    "
+    class="float-right w-full sm:w-[300px] border-y border-neutral-500 py-8 !m-10 !mr-0 text-2xl italic before:content-['“'] after:content-['”'] px-2"
   >
-    <slot />
+    <ContentSlot unwrap="p" />
   </div>
 </template>
