39058db9b659ec4d8b961193648ae1ee0aa9d23c
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-05-09 13:15:57 -0400

    Tweaks

--- components/content/Tooltip.vue
+++ components/content/Tooltip.vue
@@ -3,20 +3,32 @@ defineProps<{ text: string, content: string }>()
 
 const overlayRef = ref()
 
-function toggle(event) {
-  overlayRef.value.toggle(event)
+const buttonRef = ref()
+const tooltipRef = ref()
+
+const isButtonHovered = useElementHover(buttonRef)
+const isOverlayHovered = useElementHover(tooltipRef)
+
+function mouseOver(event: Event) {
+  overlayRef.value.show(event)
+}
+function mouseLeave() {
+  setTimeout(() => {
+    if (!isButtonHovered.value && !isOverlayHovered.value)
+      overlayRef.value.hide()
+  }, 500)
 }
 </script>
 
 <template>
-  <button @click="toggle">
+  <button ref="buttonRef" @mouseenter="mouseOver" @mouseleave="mouseLeave">
     <sup class="text-blue-500 underline">
       {{ text }}
     </sup>
   </button>
   <OverlayPanel ref="overlayRef">
     <RefTagger />
-    <div class="p-3 m-3 bg-neutral-800 max-w-[350px]">
+    <div ref="tooltipRef" class="p-3 bg-neutral-800 max-w-[350px]" @mouseleave="mouseLeave">
       {{ content }}
     </div>
   </OverlayPanel>
