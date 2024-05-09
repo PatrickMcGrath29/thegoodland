10de54bdbf931adb0bd9bdba1ffcab677cadf005
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-05-08 23:07:04 -0400

    Add tooltip hover

---
+++ components/content/Tooltip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{ text: string, content: string }>()
+
+const overlayRef = ref()
+
+function toggle(event) {
+  overlayRef.value.toggle(event)
+}
+</script>
+
+<template>
+  <button @click="toggle">
+    <sup class="text-blue-500 underline">
+      {{ text }}
+    </sup>
+  </button>
+  <OverlayPanel ref="overlayRef">
+    <RefTagger />
+    <div class="p-3 m-3 bg-neutral-800 max-w-[350px]">
+      {{ content }}
+    </div>
+  </OverlayPanel>
+</template>
