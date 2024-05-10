2c86054e31c6a1dffc5559a413d1756f9cea88c6
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-05-09 22:02:06 -0400

    Tooltip with click

--- components/content/Tooltip.vue
+++ components/content/Tooltip.vue
@@ -2,62 +2,21 @@
 defineProps<{ text: string, content: string }>()
 
 const overlayRef = ref()
-const buttonRef = ref()
-const tooltipRef = ref()
 
-const isTooltipOpen = ref(false)
-
-const isButtonHovered = useElementHover(buttonRef)
-const isOverlayHovered = useElementHover(tooltipRef)
-
-const isTooltipHovered = computed(() => {
-  return isButtonHovered.value || isOverlayHovered.value
-})
-
-function mouseEnter(event: Event) {
-  overlayRef.value.show(event)
-  isTooltipOpen.value = true
-}
-
-function shouldCloseTooltip() {
-  return isTooltipOpen.value && !isTooltipHovered.value && !isBibleTooltipOpen()
-}
-
-function mouseLeave() {
-  setTimeout(() => {
-    if (shouldCloseTooltip()) {
-      overlayRef.value.hide()
-      isTooltipOpen.value = false
-    }
-    else if (isTooltipOpen.value && !isTooltipHovered.value) {
-      const interval = setInterval(() => {
-        if (shouldCloseTooltip()) {
-          overlayRef.value.hide()
-          isTooltipOpen.value = false
-          clearInterval(interval)
-        }
-      }, 500)
-    }
-  }, 500)
-}
-
-function isBibleTooltipOpen() {
-  return document.querySelectorAll('div.rtTooltip').length > 0
+function toggleOverlay(event: Event) {
+  overlayRef.value.toggle(event)
 }
 </script>
 
 <template>
-  <button ref="buttonRef" class="p-0.5" @mouseenter="mouseEnter" @mouseleave="mouseLeave">
+  <button class="p-0.5" @click="toggleOverlay">
     <sup class="text-blue-500 underline">
       {{ text }}
     </sup>
   </button>
   <OverlayPanel ref="overlayRef">
     <RefTagger />
-    <div
-      ref="tooltipRef" class="p-4 bg-neutral-900 max-w-[400px] rounded border border-neutral-600"
-      @mouseleave="mouseLeave"
-    >
+    <div class="p-4 bg-neutral-900 max-w-[500px] rounded border border-neutral-600">
       {{ content }}
     </div>
   </OverlayPanel>
