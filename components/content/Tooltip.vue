10d10e9810a92aae98234911801329d6f9c8f173
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-05-09 20:29:05 -0400

    Hacky code

--- components/content/Tooltip.vue
+++ components/content/Tooltip.vue
@@ -6,29 +6,55 @@ const overlayRef = ref()
 const buttonRef = ref()
 const tooltipRef = ref()
 
+const isOpen = ref(false)
+
 const isButtonHovered = useElementHover(buttonRef)
 const isOverlayHovered = useElementHover(tooltipRef)
 
-function mouseOver(event: Event) {
+const isTooltipHovered = computed(() => {
+  return isButtonHovered.value || isOverlayHovered.value
+})
+
+function mouseEnter(event: Event) {
   overlayRef.value.show(event)
+  isOpen.value = true
 }
+
+function isBibleTooltipOpen() {
+  return document.querySelectorAll('div.rtTooltip').length > 0
+}
+
 function mouseLeave() {
   setTimeout(() => {
-    if (!isButtonHovered.value && !isOverlayHovered.value)
+    if (!isTooltipHovered.value && !isBibleTooltipOpen()) {
       overlayRef.value.hide()
-  }, 500)
+      isOpen.value = false
+    }
+  }, 750)
 }
+
+onMounted(() => {
+  setInterval(() => {
+    if (isOpen.value && !isTooltipHovered.value && !isBibleTooltipOpen()) {
+      overlayRef.value.hide()
+      isOpen.value = false
+    }
+  }, 750)
+})
 </script>
 
 <template>
-  <button ref="buttonRef" @mouseenter="mouseOver" @mouseleave="mouseLeave">
+  <button ref="buttonRef" @mouseenter="mouseEnter" @mouseleave="mouseLeave">
     <sup class="text-blue-500 underline">
       {{ text }}
     </sup>
   </button>
   <OverlayPanel ref="overlayRef">
     <RefTagger />
-    <div ref="tooltipRef" class="p-3 bg-neutral-800 max-w-[350px]" @mouseleave="mouseLeave">
+    <div
+      ref="tooltipRef" class="p-4 bg-neutral-900 max-w-[400px] rounded border border-neutral-600"
+      @mouseleave="mouseLeave"
+    >
       {{ content }}
     </div>
   </OverlayPanel>
