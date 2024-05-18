644bc80f3413a0f9e6491ef17c5b6373edb10e2e
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-05-18 10:26:44 -0400

    fix: collection nav refetching

--- components/CollectionNavigation.vue
+++ components/CollectionNavigation.vue
@@ -3,35 +3,31 @@ import type { Collection, Post } from '~/types'
 
 const props = defineProps<{ collection: Collection, post: Post }>()
 
-const { data: surroundingPosts } = useAsyncData(async () => {
+const { data: surroundingPosts } = await useAsyncData(`surroundingPosts/${props.post.slug}`, () => {
   const idx = props.collection.posts.indexOf(props.post.slug)
 
   const prevPostSlug = idx === 0 ? null : props.collection.posts[idx - 1]
   const nextPostSlug = idx === props.collection.posts.length - 1 ? null : props.collection.posts[idx + 1]
 
-  const [prev, next] = await Promise.all([
+  return Promise.all([
     prevPostSlug ? usePost(prevPostSlug) : null,
     nextPostSlug ? usePost(nextPostSlug) : null,
-  ])
 
-  return {
-    prev,
-    next,
-  }
+  ])
 })
 </script>
 
 <template>
-  <div class="flex justify-between mt-10 md:px-10 flex-col sm:flex-row items-center">
+  <div v-if="surroundingPosts" class="flex justify-between mt-10 md:px-10 flex-col sm:flex-row items-center">
     <div>
-      <NuxtLink v-if="surroundingPosts?.prev" class="btn btn-ghost" :to="`/posts/${surroundingPosts.prev.slug}`">
+      <NuxtLink v-if="surroundingPosts[0]" class="btn btn-ghost" :to="`/posts/${surroundingPosts[0].slug}`">
         <Icon name="mdi:arrow-left" />
-        {{ surroundingPosts.prev.title }}
+        {{ surroundingPosts[0].title }}
       </NuxtLink>
     </div>
     <div>
-      <NuxtLink v-if="surroundingPosts?.next" class="btn btn-ghost" :to="`/posts/${surroundingPosts.next.slug}`">
-        {{ surroundingPosts.next.title }}
+      <NuxtLink v-if="surroundingPosts[1]" class="btn btn-ghost" :to="`/posts/${surroundingPosts[1].slug}`">
+        {{ surroundingPosts[1].title }}
         <Icon name="mdi:arrow-right" />
       </NuxtLink>
     </div>
