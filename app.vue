923cf4415c5a68e966c50695423d31812009e649
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-06-26 13:39:37 -0400

    seo: update base title

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useHead({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `The Good Land - ${titleChunk}` : 'The Good Land'
+    return titleChunk ? `The Good Land - ${titleChunk}` : 'The Good Land Blog'
   },
   htmlAttrs: {
     lang: 'en',
