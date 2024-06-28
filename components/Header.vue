ab254d58b3afe177f9e4b7f41e3fe06a4dc8e625
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-06-28 15:23:57 -0400

    Update navbar on mobile

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,13 @@
     <Container>
       <div class="navbar bg-base-300 rounded-lg shadow-md">
         <div class="navbar-start">
-          <div class="dropdown">
+          <NuxtLink to="/" class="btn btn-ghost normal-case text-2xl flex justify-center">
+            <LogoIcon class="size-9 fill-[#ffbf00] mr-0.5 bg-neutral-700 p-1.5 rounded-md" />
+            The Good Land
+          </NuxtLink>
+        </div>
+        <div class="navbar-end">
+          <div class="dropdown dropdown-end">
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
               <Icon name="mdi:menu" size="25px" />
             </div>
@@ -14,12 +20,6 @@
               <HeaderItems />
             </ul>
           </div>
-          <NuxtLink to="/" class="btn btn-ghost normal-case text-2xl flex justify-center">
-            <LogoIcon class="size-9 fill-[#ffbf00] mr-0.5 bg-neutral-700 p-1.5 rounded-md" />
-            The Good Land
-          </NuxtLink>
-        </div>
-        <div class="navbar-end">
           <div class="hidden lg:flex">
             <ul class="menu menu-horizontal px-1 gap-1">
               <HeaderItems />
