54a4e0bc1c837028ca93e1eda0f38802d68c1bd5
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-07-10 22:59:26 -0400

    Add author and reference pages

--- pages/quotes/author/[id].vue
+++ pages/quotes/author/[id].vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
 const quoteStore = useQuoteStore()
+
 await useAsyncData('fetch-quotes', () => {
   return quoteStore.fetchQuotes()
 })
 
+const { params: { id } } = useRoute()
+
+const quotesForAuthor = computed(() => {
+  return quoteStore.quotes.filter((quote) => {
+    const authorName = quote.reference?.authorName
+    return authorName && slugify(authorName) === id
+  })
+})
+
+const author = computed(() => quotesForAuthor.value[0]?.reference?.authorName)
+
 const columnSettings = computed(() => {
-  return useColumnSettings(quoteStore.quotesForCategory.length)
+  return useColumnSettings(quotesForAuthor.value.length)
 })
 </script>
 
 <template>
   <Container>
     <h1 class="text-4xl text-center my-10 font-bold">
-      Quotes
+      {{ author }} Quotes
     </h1>
     <div class="gap-6" :class="columnSettings">
-      <div v-for="(quote, idx) in quoteStore.quotes" :key="idx" class="inline-block mb-6">
+      <div v-for="(quote, idx) in quotesForAuthor" :key="idx" class="inline-block mb-6">
         <StyledCard>
           <div class="p-6">
             <QuoteText :quote="quote" />
