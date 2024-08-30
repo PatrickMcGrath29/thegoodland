422d995f1e58581795f762cb24c3c75298f8cfb8
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-08-30 09:19:09 -0400

    style: style updates to quote header

--- components/QuoteHeader.vue
+++ components/QuoteHeader.vue
@@ -3,10 +3,13 @@ defineProps<{ heading: string }>()
 </script>
 
 <template>
-  <h1 class="my-14 flex items-center">
-    <NuxtLink to="/quotes" class="text-2xl border-r pr-4 border-neutral-500 font-light text-neutral-500">
+  <h1 class="my-10 md:my-14 flex items-center flex-col md:flex-row">
+    <NuxtLink
+      to="/quotes"
+      class="text-2xl border-neutral-500 font-light text-neutral-500 border-b md:border-r md:border-b-0 pb-1 md:pr-4 md:pb-0"
+    >
       Quotes
     </NuxtLink>
-    <span class="text-4xl pl-4 font-bold">{{ heading }}</span>
+    <span class="text-4xl pt-1 md:pl-4 md:pt-0 font-bold text-center md:text-left">{{ heading }}</span>
   </h1>
 </template>
