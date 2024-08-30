d0c3029deecac64f9d131a89027b8aa3ba3172c3
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-08-29 21:17:18 -0400

    Make header a link

--- components/QuoteHeader.vue
+++ components/QuoteHeader.vue
@@ -4,7 +4,9 @@ defineProps<{ heading: string }>()
 
 <template>
   <h1 class="my-14 flex items-center">
-    <span class="text-2xl border-r pr-4 border-neutral-500 font-light text-neutral-500">Quotes</span>
+    <NuxtLink to="/quotes" class="text-2xl border-r pr-4 border-neutral-500 font-light text-neutral-500">
+      Quotes
+    </NuxtLink>
     <span class="text-4xl pl-4 font-bold">{{ heading }}</span>
   </h1>
 </template>
