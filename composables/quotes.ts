bea551121f41f122fe3b6560116583c83c54c8e0
Patrick McGrath <patrickmcgrath29@gmail.com>
2024-09-06 09:51:02 -0400

    Refactor quote logic (#12)
    * Refactor quote logic
    
    * Fix
    
    * tweka

--- composables/quotes.ts
+++ composables/quotes.ts
@@ -1,55 +1,22 @@
-import type { Quote, RawQuote, Reference } from '~/types'
+import type { Quote, RawQuote, RawReference, Reference } from '~/types'
 
 async function useRawQuotes(): Promise<RawQuote[]> {
-  const quotes = await queryContent<RawQuote>('/quotes')
+  return await queryContent<RawQuote>('/quotes')
     .sort({ uuid: 1 })
     .find()
-
-  return quotes.map((quote) => {
-    return {
-      ...quote,
-      referenceId: quote.referenceId?.toLowerCase(),
-      uuid: quote.uuid.toLowerCase(),
-    }
-  })
 }
 
-export async function useReferences(): Promise<Reference[]> {
-  const references = await queryContent('/references')
+export async function useRawReferences(): Promise<RawReference[]> {
+  return await queryContent<RawReference>('/references')
     .sort({ uuid: 1 })
     .find()
-
-  return references.map(parseReference)
-}
-
-export function hydrateQuotes(rawQuotes: RawQuote[], references: Reference[]): Quote[] {
-  const referencesById: Map<string, Reference> = new Map(
-    references.map((reference: Reference) => [reference.uuid, reference]),
-  )
-
-  return rawQuotes.map((quote: RawQuote): Quote => {
-    const reference = referencesById.get(quote.referenceId)
-
-    const quoteSlugFields = [
-      reference?.authorName,
-      ...quote.text.split(' ').slice(0, 5),
-    ]
-
-    const quoteSlug = slugify(quoteSlugFields.filter(Boolean).join('-'))
-
-    return {
-      ...quote,
-      slug: quoteSlug,
-      reference: referencesById.get(quote.referenceId) as Reference,
-    }
-  })
 }
 
 export async function useQuotes(): Promise<Quote[]> {
-  const [rawQuotes, references] = await Promise.all([
+  const [rawQuotes, rawReferences] = await Promise.all([
     useRawQuotes(),
-    useReferences(),
+    useRawReferences(),
   ])
 
-  return hydrateQuotes(rawQuotes, references)
+  return hydrateQuotes(rawQuotes, rawReferences)
 }
