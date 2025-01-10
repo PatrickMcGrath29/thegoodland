72f2350369730a608fbd6b4d5b4a78e5cd698d79
Patrick McGrath <patrickmcgrath29@gmail.com>
2025-01-09 22:17:16 -0500

    Add shadow to quotes, update packages

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -1,6 +1,6 @@
 <template>
   <blockquote
-    class="border-l-[2px] border-accent border-opacity-50 bg-neutral-900 rounded-r-md [&>*]:m-0 p-2 pl-3 mb-7 custom-blockquote"
+    class="border-l-[2px] border-accent border-opacity-50 bg-neutral-900 rounded-r-md [&>*]:m-0 p-2 pl-3 mb-7 custom-blockquote shadow-md"
   >
     <slot />
   </blockquote>
