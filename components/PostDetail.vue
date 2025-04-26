a6081bfb9b08c15186c0e6bf09b50b61c5c506ba
Patrick McGrath <patrickmcgrath29@gmail.com>
2025-04-26 11:47:48 -0400

    style: minor tweak to post detail chips

--- components/PostDetail.vue
+++ components/PostDetail.vue
@@ -7,8 +7,16 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink class="flex items-center text-neutral-500 gap-1" :to="to || ''" :disabled="!to">
-    <Icon :name="iconName" class="pr-1.5 bg-accent opacity-50" size="20px" />
-    {{ text }}
-  </NuxtLink>
+  <template v-if="to">
+    <NuxtLink class="flex items-center text-neutral-500 gap-1 hover:underline" :to="to">
+      <Icon :name="iconName" class="pr-1.5 bg-accent opacity-50" size="20px" />
+      {{ text }}
+    </NuxtLink>
+  </template>
+  <template v-else>
+    <div class="flex items-center text-neutral-500 gap-1">
+      <Icon :name="iconName" class="pr-1.5 bg-accent opacity-50" size="20px" />
+      {{ text }}
+    </div>
+  </template>
 </template>
