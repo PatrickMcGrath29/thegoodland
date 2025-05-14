f236621f50796a588d8b44100daddcc7d7520895
Patrick McGrath <patrickmcgrath29@gmail.com>
2025-05-14 00:10:23 -0400

    feat: add command palette

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 const { data: collections } = await useAsyncData('collections', () => useCollections())
+
+const route = useRoute()
+
+const mobileMenuOpen = ref(false)
+const searchOpen = ref(false)
+
+watch(route, () => {
+  mobileMenuOpen.value = false
+})
 
 const navItems = computed(() => {
   return ([
     {
-      label: 'Blog Posts',
+      label: 'Blog',
       to: '/posts',
-    },
-    {
-      label: 'Quotes',
-      to: '/quotes',
+      icon: 'ph:scroll-duotone',
     },
     {
       label: 'Collections',
@@ -19,9 +27,80 @@ const navItems = computed(() => {
         to: `/collections/${collection.slug}`,
         description: collection.author ? `by ${collection.author}` : undefined,
       })),
+      icon: 'ph:book-open-duotone',
     },
-  ])
+    {
+      label: 'Quotes',
+      to: '/quotes',
+      icon: 'ph:pen-nib-duotone',
+    },
+    {
+      label: 'Search',
+      onSelect: () => searchOpen.value = true,
+      icon: 'ph:magnifying-glass-duotone',
+      class: 'cursor-pointer',
+    },
+  ] satisfies NavigationMenuItem[])
 })
+
+const groups = ref([
+  {
+    id: 'users',
+    label: 'Users',
+    items: [
+      {
+        label: 'Benjamin Canac',
+        suffix: 'benjamincanac',
+        avatar: {
+          src: 'https://github.com/benjamincanac.png',
+        },
+      },
+      {
+        label: 'Sylvain Marroufin',
+        suffix: 'smarroufin',
+        avatar: {
+          src: 'https://github.com/smarroufin.png',
+        },
+      },
+      {
+        label: 'Sébastien Chopin',
+        suffix: 'atinux',
+        avatar: {
+          src: 'https://github.com/atinux.png',
+        },
+      },
+      {
+        label: 'Romain Hamel',
+        suffix: 'romhml',
+        avatar: {
+          src: 'https://github.com/romhml.png',
+        },
+      },
+      {
+        label: 'Haytham A. Salama',
+        suffix: 'Haythamasalama',
+        avatar: {
+          src: 'https://github.com/Haythamasalama.png',
+        },
+      },
+      {
+        label: 'Daniel Roe',
+        suffix: 'danielroe',
+        avatar: {
+          src: 'https://github.com/danielroe.png',
+        },
+      },
+      {
+        label: 'Neil Richter',
+        suffix: 'noook',
+        avatar: {
+          src: 'https://github.com/noook.png',
+        },
+      },
+    ],
+  },
+])
+const searchTerm = ref('')
 </script>
 
 <template>
@@ -36,10 +115,29 @@ const navItems = computed(() => {
             </h3>
           </UButton>
         </div>
-        <ul class="flex px-1 gap-1 items-center">
+        <ul class="px-1 gap-1 items-center hidden md:flex">
           <UNavigationMenu :items="navItems" class="w-full justify-center" content-orientation="vertical" />
+        </ul>
+        <ul class="flex items-center md:hidden">
+          <UPopover v-model:open="mobileMenuOpen">
+            <UButton label="Open" color="neutral" variant="ghost">
+              <Icon name="i-lucide-menu" size="25px" />
+            </UButton>
+
+            <template #content>
+              <UNavigationMenu
+                orientation="vertical" :items="navItems"
+                class="w-full justify-center data-[orientation=vertical]:w-48" content-orientation="vertical"
+              />
+            </template>
+          </UPopover>
         </ul>
       </div>
     </Container>
+    <UModal v-model:open="searchOpen">
+      <template #content>
+        <UCommandPalette v-model:search-term="searchTerm" :groups="groups" placeholder="Search..." class="h-80" />
+      </template>
+    </UModal>
   </header>
 </template>
