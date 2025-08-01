92970ed8b723ce6e4c162c69e1e0bf8b6d407b9f
Patrick McGrath <patrickmcgrath29@gmail.com>
2025-08-01 09:12:57 -0400

    style: minor style tweaks to the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -43,37 +43,41 @@ async function submitSubscriptionForm(event: FormSubmitEvent<SubscriptionSchema>
   <ContainerMedium class="mb-12">
     <div class="flex justify-between flex-col md:flex-row gap-12">
       <div class="flex flex-col gap-3 items-center">
-        <div class="flex gap-2 items-center">
-          <LogoIcon class="size-9 fill-accent mr-0.5 bg-neutral-700 p-1.5 rounded-md" />
-          <h3 class="font-semibold text-2xl flex justify-center">
-            The Good Land
-          </h3>
-        </div>
-        <div class="text-base leading-tight font-semibold text-toned text-center">
-          Christ, our Good Land.
+        <div class="flex gap-3">
+          <LogoIcon class="size-9 fill-accent mr-0.5 bg-neutral-700 p-1.5 rounded-md mt-1" />
+          <div>
+            <h3 class="font-medium text-base flex justify-center max-w-72">
+              Literature that highlights the richness of life found in Jesus Christ.
+            </h3>
+            <div class="text-sm mt-2 leading-tight text-toned">
+              Christ, our Good Land.
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="flex flex-col gap-2 h-24">
-        <div class="text-sm text-medium text-toned text-center md:text-left">
-          Sign up to receive our newsletter.
-        </div>
-        <UForm
-          ref="form" :schema="subscriptionSchema" :state="subscriptionFormState" class="flex gap-2 justify-center"
-          @submit="submitSubscriptionForm"
-        >
-          <UFormField name="email">
-            <UInput
-              v-model="subscriptionFormState.email" leading-icon="ph:at-duotone" placeholder="Email"
-              class="min-w-60 md:min-w-80"
-            />
-          </UFormField>
-          <div>
-            <UButton variant="soft" type="submit">
-              Subscribe
-            </UButton>
+      <div class="flex flex-col gap-2 h-24 items-center">
+        <div class="w-fit">
+          <div class="text-sm text-medium text-toned text-left mb-2">
+            Sign up to receive our newsletter.
           </div>
-        </UForm>
+          <UForm
+            ref="form" :schema="subscriptionSchema" :state="subscriptionFormState"
+            class="flex gap-2 justify-center" @submit="submitSubscriptionForm"
+          >
+            <UFormField name="email">
+              <UInput
+                v-model="subscriptionFormState.email" leading-icon="ph:at-duotone" placeholder="Email"
+                class="min-w-60 md:min-w-80"
+              />
+            </UFormField>
+            <div>
+              <UButton variant="soft" type="submit">
+                Subscribe
+              </UButton>
+            </div>
+          </UForm>
+        </div>
       </div>
     </div>
   </ContainerMedium>
