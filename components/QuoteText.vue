ba9abeaef9be295c0893bad55963767012db33f3
Patrick Mcgrath <patrick.mcgrath@klaviyo.com>
2025-08-13 13:17:26 -0400

    style: update styles on quote card links

--- components/QuoteText.vue
+++ components/QuoteText.vue
@@ -28,7 +28,7 @@ const { quote, showPermaLink = true } = defineProps<{
       <div>
         <NuxtLink
           v-if="quote.reference?.referenceName"
-          class="text-base font-semibold hover:underline decoration-neutral-400 flex items-center gap-1.5 w-fit"
+          class="text-base font-semibold underline decoration-neutral-500 hover:text-accent flex items-center gap-1.5 w-fit"
           :to="`/quotes/reference/${quote.reference?.referenceSlug}`"
         >
           <Icon name="ph:bookmark-simple-duotone" size="15px" class="text-accent text-opacity-40" />
@@ -37,7 +37,7 @@ const { quote, showPermaLink = true } = defineProps<{
       </div>
       <div>
         <NuxtLink
-          class="text-sm italic hover:underline decoration-neutral-400 flex items-center gap-1.5 w-fit"
+          class="text-sm italic underline decoration-neutral-500 hover:text-accent flex items-center gap-1.5 w-fit"
           :to="quote.reference?.authorName ? `/quotes/author/${quote.reference?.authorSlug}` : ''"
         >
           <Icon name="ph:pen-nib-duotone" size="15px" class="text-accent text-opacity-40" />
