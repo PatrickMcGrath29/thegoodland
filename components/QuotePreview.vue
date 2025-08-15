225aa74e3f8ba2554bcd8cb40b474df449bb5d34
Patrick McGrath <patrickmcgrath29@gmail.com>
2025-08-14 22:12:53 -0400

    style: update styling on quote index page (#37)
    * style: update styling on quote index page
    
    * style: make styling consistent
    
    * fix: fix click event handler on preview cards
    
    * style: remove unnused styles
    
    * style: simplify styles
    
    * style: update quote link styles

--- components/QuotePreview.vue
+++ components/QuotePreview.vue
@@ -11,29 +11,10 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="flex-none w-[calc(100%-2rem)] md:w-96 h-80 m-0.5" @click="$emit('click', quote)">
-    <StyledCard class="h-full cursor-pointer">
-      <div class="p-3 h-full flex flex-col">
-        <div class="flex-grow overflow-hidden">
-          <p class="text-base leading-relaxed line-clamp-8 whitespace-pre-line">
-            {{ quote.text }}
-          </p>
-          <p class="text-sm text-muted mt-1">
-            Continue Reading...
-          </p>
-        </div>
-        <div
-          v-if="quote.reference"
-          class="text-sm text-neutral-400 mt-3 pt-3 border-t border-neutral-700 h-12"
-        >
-          <div v-if="quote.reference.authorName" class="font-medium">
-            {{ quote.reference.authorName }}
-          </div>
-          <div v-if="quote.reference.referenceName" class="text-xs mt-1 truncate">
-            {{ quote.reference.referenceName }}
-          </div>
-        </div>
-      </div>
-    </StyledCard>
-  </div>
+  <StyledCard
+    class="m-0.5 p-3 cursor-pointer flex-none w-[calc(100%-2rem)] md:w-96"
+    @click="$emit('click', quote)"
+  >
+    <QuoteTextWithCategories :quote="quote" preview-mode :show-full-page-link="false" />
+  </StyledCard>
 </template>
